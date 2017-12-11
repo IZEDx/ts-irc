6b6f7ddac0ce86b558754256c37b5368d574bdda
Niklas Kühtmann <admin@i-zed.net>
2017-12-11 12:50:39 +0100

    Improved entry

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,22 @@ import {log} from "./utils";
  */
 async function main(argv : string[]) {
     if (argv.length < 3) {
-        log.main("Usage: npm start -- <port>");
+        console.log("Usage: ts-irc <port>");
         return;
     }
 
     const port    = parseInt(argv[2]);
     const server  = new IRCServer(port);
 
+    // Printing empty lines to clear the screen
+    const lines = (<any>process.stdout).getWindowSize()[1];
+    for (let i = 0; i < lines; i += 1) {
+        console.log("\r\n");
+    }
+
     log.main("Starting IRC Server.");
 
-    await server.listen();
+    await server.listen(); // Starting the server and waiting for it to finish
 
     log.main("Shutdown");
 }
