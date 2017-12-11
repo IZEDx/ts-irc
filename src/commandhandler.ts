fd79877e03e3322acaa7a44feb7f438882463ad6
Niklas Kühtmann <admin@i-zed.net>
2017-12-11 19:58:02 +0100

    Added jsdoc and did some refactoring to the tell method

--- src/commandhandler.ts
+++ src/commandhandler.ts
@@ -4,6 +4,9 @@ import IRCClient from "./client";
 import {log} from "./utils";
 export {OperatorParser} from "./parser";
 
+/**
+ * Global map of functions that can be imported in a CommandHandler
+ */
 const commands : Map<string, Map<string, ICommandFunction>> = new Map();
 
 /**
@@ -16,6 +19,12 @@ export function registerCommand(target : Function, propertyKey: string, descript
     getOrDefault(commands, target.name.toLowerCase(), new Map()).set(propertyKey.toLowerCase(), target[propertyKey]);
 }
 
+/**
+ * Gets value from map or returns default, if not found.
+ * @param {Map<K, V>} map Map to get from.
+ * @param {K} key Key to get.
+ * @param {V} def Value to return if not found.
+ */
 function getOrDefault<K, V>(map : Map<K, V>, key : K, def : V) : V {
     const v = map.get(key);
     if (v === undefined) {
@@ -25,7 +34,13 @@ function getOrDefault<K, V>(map : Map<K, V>, key : K, def : V) : V {
     return v;
 }
 
+/**
+ * A class extending this can add commands to the CommandHandler
+ */
 export class CommandLib {
+    /**
+     * Returns all methods with the the @registerCommand decorator
+     */
     get commands() : Map<string, ICommandFunction> {
         return getOrDefault(commands, this.constructor.name.toLowerCase(), new Map());
     }
@@ -53,28 +68,35 @@ export default class CommandHandler implements ICommandHandler {
      * @param {IRCClient} client Client to respond to.
      */
     public async tell(msg : string, client : IRCClient) {
-        let fn : ICommandFunction | undefined;
-        let result : string | undefined;
-        const {prefix, command, args} = await this.parser.tell(msg); // this.parseByCharacter(msg, tsrue);
+        let found : {fn : ICommandFunction, lib : CommandLib}|false = false;
+        const {prefix, command, args} = await this.parser.tell(msg);
 
-        log.interaction(`${client.identifier} attempts to run ${command} ${args}.`);
+        log.interaction(`${client.identifier} attempts to run ${command}.`);
 
         for (const lib of this.libs) {
-            fn = lib.commands.get(command);
-            if (fn !== undefined) { break; }
+            const fn = lib.commands.get(command);
+            if (fn !== undefined) {
+                found = {fn : fn, lib : lib};
+                break;
+            }
         }
 
-        if (fn !== undefined) {
-            result = await fn(client, prefix, args);
+        if (!found) {
+            client.tell(client.reply.errUnknownCommand(command));
+            return;
         }
+
+        const result = await found.fn.bind(found.lib)(client, prefix, args);
 
         if (result !== undefined) {
             client.tell(result);
-        } else if (fn === undefined) {
-            client.tell(client.reply.errUnknownCommand(command));
         }
     }
 
+    /**
+     * Shuts down this CommandHandler
+     * @param sender
+     */
     public async shutdown(sender : IActor) {
     }
 }
