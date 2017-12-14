e50b5fe0ad306abf3d5dd4adb4ea5596c8b7ff6f
Niklas Kühtmann <admin@i-zed.net>
2017-12-14 20:14:59 +0100

    Added MOTD command Fixed PING and PONG command Made commands with one reply return it instead of telling the client itself

--- src/commands.ts
+++ src/commands.ts
@@ -3,6 +3,9 @@ import IRCClient from "./client";
 import {registerCommand, CommandLib} from "./commandhandler";
 import {log} from "./utils";
 import {IParseResult} from "./interfaces";
+import {readFile} from "fs";
+
+let motd : string;
 
 /**
  * Class containing some essential commands.
@@ -11,16 +14,14 @@ export class BasicCommands extends CommandLib {
     @registerCommand
     public static async NICK(client : IRCClient, cmd : IParseResult) {
         if (cmd.args.length < 1) {
-            client.tell(client.reply.errNoNicknameGiven());
-            return;
+            return client.reply.errNoNicknameGiven();
         }
 
         const oldnick = client.nick;
         const newnick = cmd.args[0];
 
         if ((await client.server.getClients("nick", newnick)).length > 0) {
-            client.tell(client.reply.errNicknameInUse(newnick));
-            return;
+            return client.reply.errNicknameInUse(newnick);
         }
 
         if (client.authed) {
@@ -40,13 +41,11 @@ export class BasicCommands extends CommandLib {
     @registerCommand
     public static async USER(client : IRCClient, cmd : IParseResult) {
         if (cmd.args.length < 1 || cmd.msg === "") {
-            client.tell(client.reply.errNeedMoreParams("user"));
-            return;
+            return client.reply.errNeedMoreParams("user");
         }
 
         if (client.authed) {
-            client.tell(client.reply.errAlreadyRegistred());
-            return;
+            return client.reply.errAlreadyRegistred();
         }
 
         if ((await client.server.getClients("username", cmd.args[0])).length > 0) {
@@ -78,8 +77,7 @@ export class BasicCommands extends CommandLib {
         const target : IRCClient = targets[0];
 
         if (targets.length === 0) {
-            client.tell(client.reply.errNoSuchNick(cmd.args[0]));
-            return;
+            return client.reply.errNoSuchNick(cmd.args[0]);
         }
 
         log.interaction(`${client.nick} > ${target.nick}\t${cmd.msg}`);
@@ -87,7 +85,45 @@ export class BasicCommands extends CommandLib {
     }
 
     @registerCommand
-    public static async PING(client : IRCClient, prefix : string, args : string[]) {
-        // TODO: client.tell()
+    public static async MOTD(client : IRCClient, cmd : IParseResult) {
+        if (!client.authed) {
+            return;
+        }
+
+        if (motd === undefined) {
+            try {
+                motd = await new Promise<string>((resolve, reject) => {
+                    readFile("./motd.txt", (err, data) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(data.toString());
+                        }
+                    });
+                });
+            } catch (err) {
+                motd = "";
+            }
+        }
+
+        if (motd === "") {
+            return client.reply.errNoMOTD();
+        }
+
+        client.tell(client.reply.motdStart().toString());
+
+        for (const line of motd.split("\n")) {
+            client.tell(client.reply.motd(line.trim()).toString());
+        }
+
+        return client.reply.endOfMotd();
     }
+
+    @registerCommand
+    public static async PING(client : IRCClient, cmd : IParseResult) {
+        return client.reply.pong();
+    }
+
+    @registerCommand
+    public static async PONG(client : IRCClient, cmd : IParseResult) {}
 }
